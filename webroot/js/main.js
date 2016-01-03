e87900859cb7bbdf746f33043cf7f6033637e35d
Artur Stachecki <artur.stachecki@gmail.com>
2016-01-02 19:31:55 -0500

    render back of user tiles

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -30,12 +30,15 @@
 })(window);
 
 (function() {
+        ModalEffects();
+
 		var menu_el = document.getElementById('menu');
         var menu = new Menu(menu_el, ajaxLoader);
 
 		var gridWrapper = document.querySelector('.content');
         gridWrapper.addEventListener('tiles-loaded', function (e) {
             TileEffects();
+            ModalEffects();
         }, false);
 
 		function ajaxLoader(ev, data_link) {
@@ -50,21 +53,8 @@
 		}
 })();
 
-function ajaxGet(data_link, target, event) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            target.innerHTML = xhttp.responseText;
-            if (event != undefined) {
-                target.dispatchEvent(event);
-            }
-        }
-    };
-    xhttp.open("GET", data_link, true);
-    xhttp.setRequestHeader("X-Requested-With",'XMLHttpRequest');
-    xhttp.send();
-}
 
+/* Tile functions */
 function replace(a, b) {
     fade(a);
     unfade(b);
@@ -101,18 +91,82 @@ function TileEffects() {
 
         var front = el.querySelector('.front');
         var back = el.querySelector('.back');
-        var tmp;
 
         front.addEventListener('mouseover', function (ev) {
             replace(front, back);
+            ev.stopPropagation();
         });
 
+        //Safe mouseout, prevents from firing functions if event happens on children, not outside of actual element
         back.addEventListener('mouseout', function (ev) {
+            var list = traverseChildren(back);
+            var e = event.toElement || event.relatedTarget;
+            if (!!~list.indexOf(e)) {
+                return;
+            }
+
             replace(back, front);
+
+            ev.stopPropagation();
         });
     });
 }
 
+//quick and dirty DFS children traversal,
+function traverseChildren(elem){
+    var children = [];
+    var q = [];
+    q.push(elem);
+    while (q.length > 0) {
+      var elem = q.pop();
+      children.push(elem);
+      pushAll(elem.children);
+    }
+    function pushAll(elemArray){
+      for(var i=0; i < elemArray.length; i++) {
+        q.push(elemArray[i]);
+      }
+    }
+    return children;
+}
+
+
+/* Modals */
+function ModalEffects() {
+    	var overlay = document.querySelector( '.modal-overlay' );
+        var modal = document.querySelector('.modal');
+        var event = new Event('modal-loaded');
+        //var close = modal.querySelector('.modal-close' );
+
+		[].slice.call( document.querySelectorAll( '.modal-trigger' ) ).forEach( function( el, i ) {
+            var data_link = el.getAttribute('data-link');
+
+			function removeModal() {
+				removeClass( modal, 'modal-show' );
+			}
+
+			function removeModalHandler() {
+				removeModal();
+			}
+
+			el.addEventListener( 'click', function( ev ) {
+                ajaxGet(data_link, modal, event);
+				addClass(modal, 'modal-show');
+				overlay.removeEventListener( 'click', removeModalHandler );
+				overlay.addEventListener( 'click', removeModalHandler );
+			});
+
+			/*close.addEventListener( 'click', function( ev ) {
+				ev.stopPropagation();
+				removeModalHandler();
+			});*/
+
+		} );
+
+}
+
+
+/* Helper functions */
 function hasClass(el, className) {
 	if (el.classList)
 		return el.classList.contains(className);
@@ -133,4 +187,19 @@ function removeClass(el, className) {
 		var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
 		el.className=el.className.replace(reg, ' ')
 	}
+}
+
+function ajaxGet(data_link, target, event) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            target.innerHTML = xhttp.responseText;
+            if (event != undefined) {
+                target.dispatchEvent(event);
+            }
+        }
+    };
+    xhttp.open("GET", data_link, true);
+    xhttp.setRequestHeader("X-Requested-With",'XMLHttpRequest');
+    xhttp.send();
 }
