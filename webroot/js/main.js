5199f829c1fd123f3e1ad3fd5046672a918c0304
Artur Stachecki <artur.stachecki@gmail.com>
2016-01-16 18:43:54 -0500

    make stuff work for ie11

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,56 +1,53 @@
-;(function(window) {
-    function Menu(el, itemFunction) {
-        this.el = el;
+/* ie fix provided by mozilla */
+(function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
 
-        // the menus (<ul>´s)
-        this.menus = [].slice.call(this.el.querySelectorAll('.menu-item'));
-        this.onItemClick = itemFunction;
-        this._init();
-    }
+  CustomEvent.prototype = window.Event.prototype;
 
-    Menu.prototype._init = function () {
-        var self = this;
+  window.CustomEvent = CustomEvent;
+})();
 
-        this.menus.forEach(function (item) {
-            item.querySelector('a').addEventListener('click', function (ev) {
-                // add class current
-                var current_link = self.el.querySelector('.menu-link--current');
-                if (current_link) {
-                    removeClass(self.el.querySelector('.menu-link--current'), 'menu-link--current');
-                }
-                addClass(ev.target, 'menu-link--current');
-
-                var data_link = item.getAttribute('data-link');
-                // callback
-                self.onItemClick(ev, data_link);
-            });
-        });
-    };
-    window.Menu = Menu;
-})(window);
 
 (function() {
+    ModalEffects();
+
+    var menu_el = document.getElementById('menu');
+    var links = [].slice.call(menu_el.querySelectorAll('.menu-item'));
+    links.forEach(function(item) {
+        item.querySelector('a').addEventListener('click', function(ev) {
+            // add class current
+            var current_link = menu_el.querySelector('.menu-link--current');
+            if (current_link) {
+                removeClass(menu_el.querySelector('.menu-link--current'), 'menu-link--current');
+            }
+            addClass(ev.target, 'menu-link--current');
+            var data_link = item.getAttribute('data-link');
+            ajaxLoader(ev, data_link);
+        });
+    });
+
+
+    var gridWrapper = document.querySelector('.content');
+    gridWrapper.addEventListener('tiles-loaded', function(e) {
+        TileEffects();
         ModalEffects();
+    }, false);
 
-		var menu_el = document.getElementById('menu');
-        var menu = new Menu(menu_el, ajaxLoader);
-
-		var gridWrapper = document.querySelector('.content');
-        gridWrapper.addEventListener('tiles-loaded', function (e) {
-            TileEffects();
-            ModalEffects();
-        }, false);
-
-		function ajaxLoader(ev, data_link) {
-			ev.preventDefault();
-			gridWrapper.innerHTML = '';
-			addClass(gridWrapper, 'content--loading');
-			setTimeout(function() {
-                var event = new Event('tiles-loaded');
-				removeClass(gridWrapper, 'content--loading');
-                ajaxGet(data_link, gridWrapper, event);
-			}, 700);
-		}
+    function ajaxLoader(ev, data_link) {
+        ev.preventDefault();
+        gridWrapper.innerHTML = '';
+        addClass(gridWrapper, 'content--loading');
+        setTimeout(function() {
+            var event = new CustomEvent('tiles-loaded');
+            removeClass(gridWrapper, 'content--loading');
+            ajaxGet(data_link, gridWrapper, event);
+        }, 700);
+    }
 })();
 
 
@@ -61,9 +58,9 @@ function replace(a, b) {
 }
 
 function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
+    var op = 1; // initial opacity
+    var timer = setInterval(function() {
+        if (op <= 0.1) {
             clearInterval(timer);
             addClass(element, 'hidden');
         }
@@ -74,9 +71,9 @@ function fade(element) {
 }
 
 function unfade(element) {
-    var op = 0.1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op >= 1){
+    var op = 0.1; // initial opacity
+    var timer = setInterval(function() {
+        if (op >= 1) {
             clearInterval(timer);
             removeClass(element, 'hidden');
         }
@@ -87,18 +84,18 @@ function unfade(element) {
 }
 
 function TileEffects() {
-    [].slice.call(document.querySelectorAll('.tile')).forEach(function (el, i) {
+    [].slice.call(document.querySelectorAll('.tile')).forEach(function(el, i) {
 
         var front = el.querySelector('.front');
         var back = el.querySelector('.back');
 
-        front.addEventListener('mouseover', function (ev) {
+        front.addEventListener('mouseover', function(ev) {
             replace(front, back);
             ev.stopPropagation();
         });
 
         //Safe mouseout, prevents from firing functions if event happens on children, not outside of actual element
-        back.addEventListener('mouseout', function (ev) {
+        back.addEventListener('mouseout', function(ev) {
             var list = traverseChildren(back);
             var e = ev.toElement || ev.relatedTarget;
             if (!!~list.indexOf(e)) {
@@ -113,19 +110,20 @@ function TileEffects() {
 }
 
 //quick and dirty DFS children traversal,
-function traverseChildren(elem){
+function traverseChildren(elem) {
     var children = [];
     var q = [];
     q.push(elem);
     while (q.length > 0) {
-      var elem = q.pop();
-      children.push(elem);
-      pushAll(elem.children);
+        var elem = q.pop();
+        children.push(elem);
+        pushAll(elem.children);
     }
-    function pushAll(elemArray){
-      for(var i=0; i < elemArray.length; i++) {
-        q.push(elemArray[i]);
-      }
+
+    function pushAll(elemArray) {
+        for (var i = 0; i < elemArray.length; i++) {
+            q.push(elemArray[i]);
+        }
     }
     return children;
 }
@@ -133,60 +131,60 @@ function traverseChildren(elem){
 
 /* Modals */
 function ModalEffects() {
-    	var overlay = document.querySelector( '.modal-overlay' );
-        var modal = document.querySelector('.modal');
+    var overlay = document.querySelector('.modal-overlay');
+    var modal = document.querySelector('.modal');
 
-		[].slice.call( document.querySelectorAll( '.modal-trigger' ) ).forEach( function( el, i ) {
-            var data_link = el.getAttribute('data-link');
+    [].slice.call(document.querySelectorAll('.modal-trigger')).forEach(function(el, i) {
+        var data_link = el.getAttribute('data-link');
 
-            // Use unique event ids, otherwise we are listening to all modals
-            var event_guid = guid();
-            var event = new Event(event_guid);
+        // Use unique event ids, otherwise we are listening to all modals
+        var event_guid = guid();
+        var event = new CustomEvent(event_guid);
 
-			function removeModal() {
-				removeClass( modal, 'modal-show' );
-			}
+        function removeModal() {
+            removeClass(modal, 'modal-show');
+        }
 
-			function removeModalHandler() {
-				removeModal();
-			}
+        function removeModalHandler() {
+            removeModal();
+        }
 
-			el.addEventListener( 'click', function( ev ) {
-                ajaxGet(data_link, modal, event);
-				addClass(modal, 'modal-show');
+        el.addEventListener('click', function(ev) {
+            ajaxGet(data_link, modal, event);
+            addClass(modal, 'modal-show');
 
-                // Close modal if user clicked oudside of it
-				overlay.removeEventListener( 'click', removeModalHandler );
-				overlay.addEventListener( 'click', removeModalHandler );
-			});
+            // Close modal if user clicked oudside of it
+            overlay.removeEventListener('click', removeModalHandler);
+            overlay.addEventListener('click', removeModalHandler);
+        });
 
-            modal.addEventListener(event_guid, function(ev) {
-                var close = modal.querySelector('.modal-close' );
-                var form = modal.querySelector('.modal-form');
+        modal.addEventListener(event_guid, function(ev) {
+            var close = modal.querySelector('.modal-close');
+            var form = modal.querySelector('.modal-form');
 
-                if (close != undefined) {
-                    close.addEventListener('click', function (ev) {
-                        ev.stopPropagation();
-                        removeModalHandler();
-                    });
-                }
+            if (close != undefined) {
+                close.addEventListener('click', function(ev) {
+                    ev.stopPropagation();
+                    removeModalHandler();
+                });
+            }
 
-                if (form != undefined) {
-                    // pass parent modal and event, allowing to control form after submission
-                    // by dispatching the very same event
-                    initializeForm(form, modal, event);
-                }
-            });
-		});
+            if (form != undefined) {
+                // pass parent modal and event, allowing to control form after submission
+                // by dispatching the very same event
+                initializeForm(form, modal, event);
+            }
+        });
+    });
 }
 
 function initializeForm(form, modal, event) {
     var submit = form.querySelector('.modal-submit');
 
     if (submit != undefined) {
-        submit.addEventListener('click', function (ev) {
-           ev.stopPropagation();
-           submitForm(form, modal, event);
+        submit.addEventListener('click', function(ev) {
+            ev.stopPropagation();
+            submitForm(form, modal, event);
         });
     }
 }
@@ -194,7 +192,7 @@ function initializeForm(form, modal, event) {
 function submitForm(form, modal, event) {
     var data_link = form.getAttribute('data-link');
     var formData = new FormData();
-    [].forEach.call(form.querySelectorAll('.input'), function( el ) {
+    [].forEach.call(form.querySelectorAll('.input'), function(el) {
         var input = el.childNodes[1];
         var name = input.id;
         var data;
@@ -219,25 +217,25 @@ function submitForm(form, modal, event) {
 
 /* Helper functions */
 function hasClass(el, className) {
-	if (el.classList)
-		return el.classList.contains(className);
-	else
-		return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+    if (el.classList)
+        return el.classList.contains(className);
+    else
+        return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
 }
 
 function addClass(el, className) {
-	if (el.classList)
-		el.classList.add(className);
-	else if (!hasClass(el, className)) el.className += " " + className
+    if (el.classList)
+        el.classList.add(className);
+    else if (!hasClass(el, className)) el.className += " " + className
 }
 
 function removeClass(el, className) {
-	if (el.classList)
-		el.classList.remove(className);
-	else if (hasClass(el, className)) {
-		var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-		el.className=el.className.replace(reg, ' ')
-	}
+    if (el.classList)
+        el.classList.remove(className);
+    else if (hasClass(el, className)) {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+        el.className = el.className.replace(reg, ' ')
+    }
 }
 
 function guid() {
@@ -260,7 +258,7 @@ function ajaxGet(data_link, target, event) {
         }
     };
     xhttp.open("GET", data_link, true);
-    xhttp.setRequestHeader("X-Requested-With",'XMLHttpRequest');
+    xhttp.setRequestHeader("X-Requested-With", 'XMLHttpRequest');
     xhttp.send();
 }
 
@@ -275,6 +273,6 @@ function ajaxPost(data_link, target, form, event) {
         }
     };
     xhttp.open("POST", data_link, true);
-    xhttp.setRequestHeader("X-Requested-With",'XMLHttpRequest');
+    xhttp.setRequestHeader("X-Requested-With", 'XMLHttpRequest');
     xhttp.send(form);
 }
